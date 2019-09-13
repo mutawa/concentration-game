461148af9bf37155682f2bd5e2441693050b75dd
Ahmad Al-Mutawa <ahmad.mutawa@gmail.com>
2019-09-13 19:22:23 +0300

    enhanced card design and added animation

--- js/app.js
+++ js/app.js
@@ -16,14 +16,14 @@ let secondsCounter = 0;
 let timerHandler;
 
 document.getElementById("btnStart").addEventListener("click", function(){
-    clearGrid();
+
     generateRandomCards();
     startCounter();
 });
 function clearGrid() {
     let cards = document.querySelectorAll(".card");
     for(card of cards) {
-        card.removeEventListender('click', cardClicked);
+        card.removeEventListener('click', cardClicked);
     }
     document.getElementById("game-board").innerHTML = "";
 }
@@ -39,14 +39,20 @@ function generateRandomCards() {
                  "grin", "grin"];
 
     let board = document.getElementById("game-board");
+    board.innerHTML = "";
+
     for(let y=0; y<4; y++) {
         for(let x=0; x<4; x++) {
             let card = document.createElement("div");
+            let text = document.createElement("span");
+            card.appendChild(text);
+
             card.classList.add("card");
+            card.classList.add("closed");
             card.classList.add("fas");
             let index = parseInt(Math.random() * icons.length);
             let icon = icons.splice(index, 1);
-            card.classList.add("fa-" + icon);
+            text.classList.add("fa-" + icon);
             card.addEventListener('click', cardClicked);
             //card.innerText = `x:${x}, y:${y}`;
             board.appendChild(card);
@@ -55,29 +61,61 @@ function generateRandomCards() {
 }
 
 function cardClicked() {
-    let flippedCards = document.querySelectorAll(".card.flipped");
     
+    let flippedCards = document.querySelectorAll(".card.flipped").length;
+
+    // check if current clicked card is already flipped open
+    if(this.classList.contains("flipped")) {
+        // it is? then just ignore the click, and early-exit this function
+        return;
+
+    // if it is not, then make sure we have less than 2 cards flipped open
+    } else if(flippedCards<2) {
+        
+        // we need to make sure that no more than only 2 cards 
+        // are flipped open at any time. 
+
+        this.classList.toggle("flipped");
+        this.classList.toggle("closed");
+
+        // since the current card has just been manually flipped, 
+        // the count should be updated.
+        // however, we can't just use querySelectorAll too soon.
+        // that's why we will just add 1 to it to do its job.
+        flippedCards += 1;
+        // if we have 2 cards open, then we are ready to check if they are a match
+        if(flippedCards == 2) {
+            // wait 1 second for the user to register the card (visual feedback)
+            // then call checkMatchingCards function which will do the comparision
+            setTimeout(checkMatchingCards, 1000);
+        }
+        
+    }
+
+    
+}
+
+function checkMatchingCards() {
+    let flippedCards = document.querySelectorAll(".card.flipped");
 
     if(flippedCards.length === 2) {
 
         for(card of flippedCards) {
+            card.classList.toggle("closed");
             card.classList.toggle("flipped");
         }
     }
-    this.classList.toggle("flipped");
-    
 }
 
 function startCounter() {
+    clearInterval(timerHandler);
+
     timerHandler = setInterval(function(){
         secondsCounter += 1;
         timer.innerText = secondsToTimeFormat(secondsCounter);    
     },1000);
 }
 
-function stopCounter() {
-    clearInterval(timerHandler);
-}
 
 function secondsToTimeFormat(secondsCounter) {
     let hours = parseInt(secondsCounter / (60*60));
